8b55b3c88ffb3a52f5ae460d0e49fc198109713c
Aiden gillespie <aiden@cold.org>
2021-08-31 13:56:28 -0600

    Updated boiler plate

--- server.js
+++ server.js
@@ -2,5 +2,24 @@ const express = require("express")
 const path = require("path")
 // const Rollbar = require("rollbar")
 
+let rollbar = new Rollbar({
+  accessToken: "a9512868c7d042e9a5f3b86f0d82af42",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 const app = express()
-app.use('/style', express.static('/public/styles.css'))
+app.use("/style", express.static("/public/styles.css"))
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/server/index.html"))
+//   rollbar.info(
+//     "html file served like a good steak entree at an upscale restraunt"
+//   )
+// })
+
+app.listen(4040, () => console.log("Loud and Clear on 4040"))
+
+const port = process.env.PORT || 4040
+
+app.listen(port, () => console.log(`Loud and clear on ${port}`))
