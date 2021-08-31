9d1858183c97bc8f20f9f810c54f295270cd84ae
Aiden gillespie <aiden@cold.org>
2021-08-31 14:01:22 -0600

    update 0.0.1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const path = require("path")
-// const Rollbar = require("rollbar")
+const Rollbar = require("rollbar")
 
 let rollbar = new Rollbar({
   accessToken: "a9512868c7d042e9a5f3b86f0d82af42",
@@ -9,7 +9,13 @@ let rollbar = new Rollbar({
 })
 
 const app = express()
-app.use("/style", express.static("/public/styles.css"))
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, "/server/index.html"))
+  rollbar.info(
+    "html file served like a good steak entree at an upscale restraunt"
+  )
+})
+// app.use("/style", express.static("/public/styles.css"))
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "/server/index.html"))
@@ -18,7 +24,7 @@ app.use("/style", express.static("/public/styles.css"))
 //   )
 // })
 
-app.listen(4040, () => console.log("Loud and Clear on 4040"))
+// app.listen(4040, () => console.log("Loud and Clear on 4040"))
 
 const port = process.env.PORT || 4040
 
